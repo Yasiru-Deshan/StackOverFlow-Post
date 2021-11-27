07ece000a234f7302a21d31050a95cbfc6f0b835
yassa98 <yasirudeshan3@gmail.com>
2021-11-27 21:56:09 +0530

    first commit

---
+++ Classes-Exercise 2/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Exercise_2
+{
+    public class Post
+    {
+        private String title;
+        private String description;
+        private DateTime time;
+        private String vote;
+        private int upVote, downVote,votes;
+
+        public void CreatePost()
+        {
+            Console.WriteLine("Enter Title : ");
+            title = Console.ReadLine();
+
+            Console.WriteLine("Enter Description : ");
+            description = Console.ReadLine();
+
+            Console.WriteLine("Enter U to up-vote.. Enter D to down-vote.. ");
+            vote = Console.ReadLine();
+
+            while (vote == "u" || vote == "d")
+            {
+                if (vote == "u")
+                {
+                    upVote++;
+                 
+                }
+                else if (vote == "d")
+                {
+                    downVote++;
+   
+                }
+                vote = Console.ReadLine();
+            }
+
+            if (upVote > downVote)
+            {
+                votes = upVote - downVote;
+            } else if (downVote > upVote)
+            {
+                votes = downVote - upVote;
+            } 
+
+            Console.WriteLine("Title : {0}", title);
+            Console.WriteLine("Description : {0}", description);
+            Console.WriteLine("Up Votes : {0}", upVote);
+            Console.WriteLine("Down Votes : {0}", downVote);
+            Console.WriteLine("Votes : {0}", votes);
+            Console.WriteLine("Post created at : {0}", DateTime.Now.ToString());
+
+
+        }
+
+
+
+    }
+}
---
+++ Classes-Exercise 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Classes_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newPost = new Post();
+            newPost.CreatePost();
+        }
+    }
+}
